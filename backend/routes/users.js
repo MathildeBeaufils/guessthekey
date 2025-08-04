d6937244f52f872cbee28871badec943c8091da2
Léo <leolepinay@gmail.com>
2025-08-04 14:43:52 +0200

    routes user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,72 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+require('../models/connection');
+const User = require('../models/users');
+const { checkBody } = require('../modules/checkBody');
+const uid2 = require('uid2');
+const bcrypt = require('bcrypt');
+
+
+
+// route POST (/user/signup) pour créer un nouvel utilisateur
+router.post('/signup', (req, res) => {
+  if (!checkBody(req.body, ['email','username', 'password'])) {
+    res.json({ result: false, error: 'Champs manquants ou vides' });
+    return;
+  }
+  // création nouvel utilisateur après avoir checké que le nom d'utilisateur n'existe pas déjà
+  User.findOne({ username: req.body.username }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        username: req.body.username,
+        created_at: Date,
+        email: req.body.email,
+        password: hash,
+        token: uid2(32),
+        isAdmin: false,
+        nbVictoire: 0,
+        keyPoint: 0,
+        itemTete: '',
+        itemTorse: '',
+        itemJambes: '',
+        itemPieds: '',
+      });
+
+      newUser.save().then(data => {
+        res.json({ result: true, token: data.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: "Le nom d'utilisateur existe déjà" });
+    }
+  });
+})
+
+// route POST (/user/signin) pour se connecter
+router.post('/signin', (req, res) => {
+  if (!checkBody(req.body, ['email', 'password'])) {
+    res.json({ result: false, error: 'Champs manquants ou vides' });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then(data => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: 'Utilisateur introuvable ou mot de passe incorrect' });
+    }
+  });
 });
+
+// route DELETE (/user/delete) pour supprimer un utilisateur
+router.delete('/deleteUser', (req, res) => {
+  User.deleteOne({ email: req.body.email }).then(user => {
+    res.json({ result: true, deletedUser: user, message: 'Utilisateur supprimé' });
+  })
+})
+
 
 module.exports = router;
