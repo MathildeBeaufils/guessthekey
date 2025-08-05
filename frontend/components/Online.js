902cb64104a6993a100908676b7423a50913111e
LAMISSE <tom.lamisse@gmail.com>
2025-08-05 16:36:04 +0200

    onlinePage pas terminée

---
+++ frontend/components/Online.js
@@ -0,0 +1,25 @@
+import styles from "../styles/Online.module.css";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+function Online() {
+  return (
+    <>
+      <div className={styles.container}>
+        <h1 className={styles.create}>En ligne</h1>
+        <p className={styles.game_search}>Recherche de parties</p>
+        <div className={styles.game_container}>
+          {/* insérer un .map ici quand la création de manche sera faite */}
+          <div className={styles.game_user}>
+            <p>Username</p>
+            <p>Catégorie</p>
+            <p>1/8</p>
+            <button className={styles.button_join}> Join</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Online;
