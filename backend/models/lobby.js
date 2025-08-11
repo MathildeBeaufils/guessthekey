e6ee16701fe879cf154e16389bbcbd9f45c5ac97
Léo <leolepinay@gmail.com>
2025-08-11 09:10:00 +0200

    no change

--- backend/models/lobby.js
+++ backend/models/lobby.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const LobbySchema = new mongoose.Schema({
   code: { type: String, unique: true, required: true },
-  players: [{username: { type: String, required: true }}],
+  players: [{username: { type: String }}],
   createdAt: { type: Date, default: Date.now, expires: 3600 }, // 1h expiration
 });
 
