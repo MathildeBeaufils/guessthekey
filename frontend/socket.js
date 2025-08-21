57cc403728f98eb5f3d19cccb53cc3e1f90c6f5d
Guillaume MEYER <guimeyer64@gmail.com>
2025-08-21 18:05:48 +0200

    fix socket url

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,5 +1,10 @@
 import { io } from "socket.io-client";
 // port déterminé dans le backend, spécifique pour le Socket
-const socket = io("https://guessthekey.vercel.app");
+const socket = io("https://guessthekey.onrender.com", {
+transports: ["websocket", "polling"], // compatible avec Render
+withCredentials: true
+});
+
+
 
 export default socket;
