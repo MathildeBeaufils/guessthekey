783acbda0f4e74c5b9ca64d3769a97fe174295e1
Guillaume MEYER <guimeyer64@gmail.com>
2025-08-21 19:53:15 +0200

    polling uniquement

--- frontend/socket.js
+++ frontend/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 // port déterminé dans le backend, spécifique pour le Socket
 const socket = io("https://guessthekey.onrender.com", {
-transports: ["websocket", "polling"], // compatible avec Render
+transports: ["polling"], // compatible avec Render
 withCredentials: true
 });
 
